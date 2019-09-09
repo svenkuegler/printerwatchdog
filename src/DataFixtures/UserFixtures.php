ff8224d6e603b84c3a4415f4a563c0425cfdfa6e
Sven Kuegler <sven.kuegler@gmail.com>
2019-09-09 11:03:42 +0200

    update fixtures to create local and ldap user

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -35,6 +35,7 @@ class UserFixtures extends Fixture
             ->setRoles(['ROLE_USER', 'ROLE_ADMIN'])
             ->setEmail('admin@printerwatchdog.org')
             ->setPassword($this->passwordEncoder->encodePassword($userAdmin,'123456'))
+            ->setSource('local')
             ;
         $manager->persist($userAdmin);
 
@@ -46,6 +47,7 @@ class UserFixtures extends Fixture
             ->setRoles(['ROLE_USER'])
             ->setEmail('testuser@printerwatchdog.org')
             ->setPassword($this->passwordEncoder->encodePassword($user,'123456'))
+            ->setSource('local')
         ;
         $manager->persist($user);
 
@@ -57,9 +59,20 @@ class UserFixtures extends Fixture
             ->setRoles(['ROLE_USER'])
             ->setEmail('testuser-inactive@printerwatchdog.org')
             ->setPassword($this->passwordEncoder->encodePassword($userInactive,'123456'))
+            ->setSource('local')
         ;
         $manager->persist($userInactive);
 
+        // Create Normal LDAP User
+        $ldap_user = new User();
+        $ldap_user->setUsername("ldapuser@mydomain.local")
+            ->setIsActive(true)
+            ->setRoles(['ROLE_USER'])
+            ->setEmail('ldapuser@printerwatchdog.org')
+            ->setPassword(null)
+            ->setSource('ldap')
+        ;
+        $manager->persist($ldap_user);
 
         $manager->flush();
     }
